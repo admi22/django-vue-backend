7e968621a4e71311d4fe379edbbcbdb9745e8485
michi <michi.blum@hotmail.com>
2021-08-28 13:13:15 +0200

    add Pets model and migration

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=60)
+    birthdate = models.DateField(default=None, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
