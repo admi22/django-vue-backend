57488985d5eeee890199ca76b12351fc4ff0ff9d
michi <michi.blum@hotmail.com>
2021-08-28 21:10:45 +0200

    add endpoint for obtaining csrf token

--- pets/urls.py
+++ pets/urls.py
@@ -4,6 +4,7 @@ from . import views
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('csrf/', views.csrf),
     path('test/', views.test),
     path('pets/', views.pets),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core import serializers
+from django.middleware.csrf import get_token
 from .models import Pet
+
+def csrf(request):
+    return JsonResponse({'csrfToken': get_token(request)})
 
 def test(request):
     # data = json.loads(request.body)
