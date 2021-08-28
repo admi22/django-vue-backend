fd8479e1eb18499edf37eeab11b3b2f5b3286a9e
michi <michi.blum@hotmail.com>
2021-08-28 17:53:09 +0200

    test url and view endpoint

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def test(request):
+    # data = json.loads(request.body)
+    # is_mobile = data.get('isMobile')
+
+    response = JsonResponse({'ok': True})
+
+    # # check if user already has session
+    # if request.session:
+    #     # TODO: decide what to do if session already exists.
+    #     #  delete session?
+    #     request.session.endTime = timezone.now()
+    #
+    # session = SessionService.create_new_session()
+    # session.ip = SessionService.get_client_ip(request)
+    # session.isMobile = is_mobile
+    # session.save()
+    # # store session id as cookie on the client
+    # response.set_cookie('session_id', session.sessionId)
+
+    return response
